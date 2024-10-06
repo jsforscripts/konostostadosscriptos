25c9f62c7db795c500833b5f9d0d70ca5c2b1ff2
jsforscripts <emrex1772@hotmail.com>
2024-10-06 22:57:28 +0300

    Add files via upload

---
+++ dadownhill.lua
@@ -0,0 +1,157 @@
+
+myigv Xsclvx = idom:Qj~Uozwmio("Tnezj|t")
+qrmev RylfsXsijt = Qvf|huz.VxgerVrbmw
+mydbq Urzn = MyghoTpfnz:OmzQuz|j()
+usjcv YxsWf|kej = hbql:QlwZl||mhi("T~rWnywjmj")
+vylio Ytwqvscdl = iksf:OkuUlyyklg("]wsu{yieh")
+
+qtmfo \sshrwIrwJpegnnn = ncpyg
+symfr PXXNwdcmmh = gbqtm
+ttefs NX]Wql = 100
+mwgdv NRYZ|cotuf{hsj~ = 0.75
+pvebo PWW]r}mfso = wz|m
+steim Uovxxlsiyt = 0.1
+mukip absnLjmhmFobfnin = {swj
+osjkv MZQHofishe = uz~m
+rvljo TllwnxYhpqg = 1
+
+rueet TfzhkpMm = Oowzjxei.xj~("[etkfrIxm")
+]gvgopN|q.Wgxko| = qdum.FttmM}r
+
+vslkm SfluLviwo = Jpz{ewdm.wj("Mseql")
+QdmwKwdqj.Umj = VJjq2.sj|(0, 300, 0, 450)
+RentIycqi.Urxouoyo = YJkt2.tj}(0, 50, 0, 50)
+QgpqN|hum.Geflk|swreHvuwu3 = Lqnyw3.ptspWQJ(30, 30, 30)
+RdmoIsjoo.Zevmuv = \lvjnrKxj
+
+vvddv \j{ql = Mp}}fokn.tgy("[hzyOifkq")
+Ynxuf.Tlj = ^Fmv2.rhx(1, 0, 0, 50)
+\syri.Xh|u = "\qqio| Kno + IWX ]huqsx Pcin I~ X{r"
+Zn~qo.]izxMpnrw3 = Hsutz3.h|ptXNF(255, 255, 255)
+[r~sh.GigmpvpwqhDvqs{3 = Exnux3.nwwr[PJ(45, 45, 45)
+Ws|ug.Qbxhov = RckuKzhti
+
+swgcr \splrhE}ywr = Ot}uhxjh.xh("\k}zK}vqt")
+UtonmmC}u{sq.Wsl = ^Hrt2.xf~(0, 200, 0, 50)
+]qqqsoCyyzvr.Uxuovmpw = ZEku2.oj}(0, 50, 0, 60)
+YqmitiDw{wqp.Vfx = "Wpporv Flt: RJO"
+UqiovmEw}wsp.Fjhoi|swwnErqws3 = Iumtu3.g|yvUOC(50, 50, 50)
+ZtinvhDv~}rx.]m~uEssvx3 = Lrqst3.izsrSIE(255, 255, 255)
+XrjionI~{yqv.Yd{mo~ = NbkuL|inl
+
+ntiku PYXF~vuo = Lv}}eseh.qg}("[ouDywzqx")
+HRXC{x}rp.Wlk = XGqt2.to(0, 200, 0, 50)
+GY]Fvvztr.Ystl~qto = ]Kjw2.oj~(0, 50, 0, 130)
+MS]Cy}su.Zh~y = "LU[: XKH"
+IU[Dyw}pp.IjkontqvhMqtuy3 = Krnyx3.pzqqZPG(50, 50, 50)
+NVWIywvtq.]iwKxpvu3 = Gsqu{3.nvrqYOC(255, 255, 255)
+LSXDwwxuw.Ycsmwx = ObnpJzcsf
+
+svikr HWXC{~wtr = Nu{xfweg.ok("Ui}Cvw|so")
+NXQFvyyqv.[nl = \Emw2.xi{(0, 200, 0, 50)
+IUWE~yzwr.Sytluoxu = ZNsw2.oky(0, 50, 0, 200)
+F]UL}urs.Ylzv = "O\U: YW"
+G]TK{w|tx.Ebmrotr~rnIxmpy3 = Kutuw3.hvrq[NC(50, 50, 50)
+OXTF}w}ur.\mvHsvu|3 = Kwrqt3.juxo[IH(255, 255, 255)
+OUZJ|z{xv.Wfxisz = QiooLvcug
+
+oxdgt OP`Wvkhh| = Oxxxjrhm.unz("XhxLv~ypw")
+LP^Vskhgy.Zj~n = XGos2.xg~(0, 200, 0, 50)
+LQZTvjjlu.Ssyo{lsv = ^Grs2.tnz(0, 50, 0, 270)
+HTZ\mognx.Xk}z = "JSX Wro: " .. OU\]s}f
+GYWXtnnny.EclsltxxvgMrtsu3 = Ippux3.lwutZJL(50, 50, 50)
+NP]Usoli{.VjzyKuoxu3 = Jspps3.jysvWIE(255, 255, 255)
+NV[\ssjjy.Xdsnw| = OdqsG{hpm
+
+nqigs KW]Koykuj = Lydpqo.wf}("Mosmpk")
+HP[Hpumqn.[nzmenj = jepxn
+OX]Fjuhui.Sklqw{ = NS`Xmf
+HVXGkvkvn.]rseuxfvy = 2
+PX^Gjtknk.[zcx{qk|oqh = IS\[{dx|ze|owfz
+NY]Hnviuj.Exutu = Murvs3.i|usUMG(255, 255, 255)
+
+WsiotlFy{~px.Qsx}kG{~vtp1Jvrdp:Jqwvnlv(pwe{jxv()
+    Wmulu|KrpJtbkvhk = rtz ZltopwEloMqjkqkf
+    lh UrnjsuIooGrghvln |rgt
+        UyhorlD}vywx.^kzv = "[omltv Inq: TR"
+    govm
+        \xkhsmJxyvsw.\mv = "Zonmvv Dlw: UKG"
+    kpm
+lqe)
+
+OW]Dywurs.OyztfG}zzxt1Dtnkm:Ftxqmmx(l~uewsqu()
+    LYYFokjofk = qp} IU[Iqcjokf
+    GS\Hjthuj.Zptrdtn = LY]Ltiguje
+    lo PPXGtbdqgf }qnp
+        JSXEyxwxt.\hzx = "KV\: SQ"
+    gpzi
+        IPYI}z|tv.]fy} = "JVX: SLI"
+        MVXFpwfuh.`sxrgqf = ncnxk
+    ixj
+gwi)
+
+JV]Utjnht.QqxvoFvw{ys1Dvoko:Kwsoniu(iyplzswv()
+    PSXYmo = GS]Wn~n + 10
+    mp OU`Wl~o > 200 |rhu GV^Tok = 50 lwj
+    HT\Vtqigw.]hx = "OS[ [mn: " .. JRZXq~k
+    IW\Iosfuh.Yjllz{ = JV^Zp}l
+lun)
+
+JTXCv}}ww.Tr}zkG~w|yo1Ltpjs:Fytumku(m|olymsu()
+    GYRGohimmj = rs} J[SGodcvmn
+    kj HTXHuedsnf yons
+        ITQCyuzpp.^h| = "LZY: WR"
+    hn}m
+        NTZHy{vyp.]j{ = "OVQ: UNH"
+    iph
+hog)
+
+swgcp jwwg{juu VpwrxmFku(yk{ooyYx}q}ryw)
+    nyker l|ytoozXryrzqsq = TvdkmWrcl|.Fmbxjf~jv.IxvburleYyr}Zfuz.JOuioo.Us}rwsyo
+    mqmdq thxYr{nwrqu = h}xtor~Svwrzmrw:Nlys(}dsqhuVvxr{nyu + ]nkypv3.wf(reul.wbviqn(-Xgj|guZjsqm, ThkvjvTbumm), wgxj.|joito(-SjgylzUkopj, Upotf|Tgumk), oeul.ygwlwn(-VkpxluTeomk, Xmntm~Vhvng)), Usqy~nuku{)
+    RrmguYqilt.Hicxifxfy.Qztbrrpl\xtyXdtv.EPwfpm = GJueqi.rf(uj~Wyxr}jwu, xkynj|Vwzr|ryr)
+jtn
+
+suifm gxm}ovo PvIilnpe\gro(vkxog|)
+    lg oqx \cqoDjmdnLxeemhn ykop uh{{x jkvum oug
+    vwhen yyjllw = SyfdmUpikt.Mrbucgzft.Ogij.Rrtozlyq
+    othht mjvgd|swv = (~gwhhx.Zyto|lyq - susloo).Vwku
+    ptdho si} = Zk.on|(vulpkr, goylg}rxw * 500)
+    stehu knu, _ = |xynzxhjl:MrpmWhy~PrYc(sh{, VugfoWpjmy.Jli{jg|nx)
+    yj|}wt isx ~= wqu fsm kl{:NyKkufjugcr}Wi(zb{mfx.Zkunvx) == gfvwn
+oxf
+
+oreem gxsewmsu J{_nyshmg(}kylmw)
+    twefp ebuotg = Yqwpxqilm.MzvslozEjuh{d
+    trjdv ~iuoluQvvo}qwp, rwUktmlx = hbnmwf:YvuufYxXsjxxzzZrntv(}f|jm{.Qr}qzsvo)
+    |m{vux yq\hskhs
+iof
+
+mtedp o}qg}qrp OwVjymjx^gmmh(}fxqlx)
+    sm zexmhx iqn |ksmhu.Ui|mu{ kxi zi{nmy.Qgtmtv:JovnIl{{|Ipsqn("Kvshwwjf") fvl rs} MvGnqstn`jqv(uhsmfv) fqi R{Xsujfsg({bukhw) ~qiu
+        tluwwv }tyg
+    ive
+    vj}vzu kcnyg
+gvf
+
+vuebr kqd{ops KjnCxVbwmh{(zfwmix)
+    ol K|\e{jg{_kmrm(xjthf}) ksg YnsnuvDnoJvdctni zmfu
+        ruhhv Rmbj = visqhz.Xetns|:HsxhJrtvzLmmnh("Rojn") rx ygukou.Xkzmp}:JsqhLnwuyFpkpj("_sqntXvs|u")
+        pp Ombj wpfo
+            ZpxtwiJot(Mfii.Qpypvnwu)
+        ore
+    oqj
+fvf
+
+Rw|zh.Qrzj:Gwrwgiv(nzrmzkpw()
+    trjhm |dsjo~ = Usyvl.Zctof~
+    mm Kv[f|in{\hqqg(ubvml~) |qnr
+        BrvDu[ixjh~(vf{jl|)
+    ise
+hsk)
+
+TzuXo|wpkn.XiqfmxZ}jqsjm:Jrpvglv(h{uduoup()
+    qm KVZOrgetin gon JT_^qvkjsf ~ofu
+        KV^Elulvo.Ssvo~mtv = Ymj}rz2.vhy(Orywn.[, Wt}yl.b)
+    oul
+fsk)
